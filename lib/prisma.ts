518a7da9dcf7d1f4b4cb14be626b5e704c16228f
dinug-c <resmaadi@gmail.com>
2023-05-10 14:46:42 +0700

    fix prisma type error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,25 +1,17 @@
-import { Prisma, PrismaClient } from "@prisma/client";
-
-declare global {
-  namespace NodeJS {
-    interface Global {
-      prisma: PrismaClient;
-    }
-  }
-}
+import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
 
-if (typeof window === "undefined") {
-  if (process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient();
-  } else {
-    if (!global.prisma) {
-      global.prisma = new PrismaClient();
-    }
-
-    prisma = global.prisma;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient;
+  };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
   }
+  prisma = globalWithPrisma.prisma;
 }
 
 export default prisma;
