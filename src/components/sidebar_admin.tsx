3c54dd5469354eb67bedf53032452ddd7c8d40da
kristiandavidd <kristiandavid644@gmail.com>
2023-05-13 06:36:32 +0700

    update sidebar

--- src/components/sidebar_admin.tsx
+++ src/components/sidebar_admin.tsx
@@ -6,15 +6,15 @@ import { Home, Puzzle, Book2, School } from 'tabler-icons-react'
 
 export default function SidebarAdmin() {
     return (
-        <div className='flex-col items-center hidden w-1/4 h-screen p-10 mb-20 bg-white lg:mb-0 lg:flex'>
+        <div className='flex-col items-center hidden h-screen p-10 mb-20 bg-white w-1/8 lg:mb-0 lg:flex'>
             <img src="../../logo-dcf.svg" alt="" className='w-3/5 px-5 py-2 rounded-md bg-dcf-brown/60'/>
             <div>
-                <div className='flex items-center px-2 py-3 m-auto my-10 rounded-md w-6/7 items bg-dcf-light-brown/60 h-[70px]'>
+                <div className='flex items-center px-2 py-3 m-auto my-10 rounded-md w-[200px] items bg-dcf-light-brown/60 h-[70px]'>
                     <div className='h-full w-[40px] bg-cover mx-3' style={{backgroundImage: "url('../../mascot2.svg')",}}>
                     </div>
                     <div>
                         <p className='text-sm font-medium'>Askari</p>
-                        <p className='pr-5 text-sm'>Admin DCF 2023</p>    
+                        <p className='pr-5 text-sm'>Admin DCF</p>    
                     </div>
                 </div>
                 <div>
@@ -38,7 +38,7 @@ export default function SidebarAdmin() {
                             </Link>
                         </li>
                         <li>
-                            <Link href='/admin/seminar' className='flex items-center p-2 my-3 rounded-full hover:bg-dcf-light-brown/60'>
+                            <Link href='/admin/semnas' className='flex items-center p-2 my-3 rounded-full hover:bg-dcf-light-brown/60'>
                                 <School strokeWidth={1.5} className="mx-2"/>
                                 Seminar
                             </Link>
