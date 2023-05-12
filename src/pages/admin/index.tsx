a0b56143105e7fae5567dee8a21a4a918cb4efe3
kristiandavidd <kristiandavid644@gmail.com>
2023-05-13 00:31:02 +0700

    update admin dashboard

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -2,58 +2,16 @@ import React from 'react'
 import { EmptyLayout } from '@/components/layout'
 import Sidebar from '@/components/sidebar'
 import Link from 'next/link'
+import SidebarAdmin from '@/components/sidebar_admin'
 
 export default function index() {
     return (
         <EmptyLayout pageTitle='Admin'>
-            <div className='flex-col items-center hidden w-1/4 h-screen p-10 mb-20 bg-white lg:mb-0 lg:flex'>
-                <img src="../../logo-dcf.svg" alt="" className='w-3/5 px-5 py-2 rounded-[20px] bg-dcf-brown/60'/>
-                <div>
-                    <div className='flex items-center px-1 py-2 m-auto my-10 rounded-full w-6/7 items bg-dcf-light-brown/60 h-fit'>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-12 h-12 mx-2 text-dcf-dark-brown">
-                            <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
-                        </svg>
-                        <div>
-                            <p className='text-sm font-medium'>Rozy Rodriques</p>
-                            <p className='pr-5 text-sm'>kristiandavid644@gmai.com</p>    
-                        </div>
-                    </div>
-                    <div>
-                        <ul>
-                            <li>
-                                <Link href="/dashboard" className="flex items-center p-2 my-3 rounded-full hover:bg-dcf-light-brown/60">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 mx-2">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                                    </svg>
-                                    Beranda
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href='/dashboard/perlombaan' className='flex items-center p-2 my-3 rounded-full hover:bg-dcf-light-brown/60'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 mx-2">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 01-.657.643 48.39 48.39 0 01-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 01-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 00-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 01-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 00.657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 01-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 005.427-.63 48.05 48.05 0 00.582-4.717.532.532 0 00-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 00.658-.663 48.422 48.422 0 00-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 01-.61-.58v0z" />
-                                    </svg>
-                                    Perlombaan
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href='/dashboard/semnas' className='flex items-center p-2 my-3 rounded-full hover:bg-dcf-light-brown/60'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 mx-2">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
-                                    </svg>
-                                    Seminar
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href='/dashboard/profil' className='flex items-center p-2 my-3 rounded-full hover:bg-dcf-light-brown/60'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 mx-2">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-                                    </svg>
-                                    Profil
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
+            <div className="w-full bg-center bg-auto lg:w-screen lg:h-screen h-fit" style={{
+            backgroundImage: "url('../../bgform.svg')",
+            }}>
+                <div className="flex w-full h-full backdrop-blur-3xl">
+                    <SidebarAdmin/>
                 </div>
             </div>
         </EmptyLayout>
