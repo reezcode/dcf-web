0bf60f4b2b9a82ad4e78a334b47d0e44b48c2611
kristiandavidd <kristiandavid644@gmail.com>
2023-05-12 09:38:05 +0700

    update lkti

--- src/pages/dashboard/lkti.tsx
+++ src/pages/dashboard/lkti.tsx
@@ -4,10 +4,11 @@ import NavDashboard from '@/configs/navigation_dashboard'
 import NavButton from '@/components/nav_button'
 import { EmptyLayout } from '@/components/layout'
 import { TextInput } from '@mantine/core'
-import { At, FileUpload, School, Tex, UserCircle, BrandWhatsapp } from 'tabler-icons-react'
+import { At, FileUpload, School, Tex, UserCircle, BrandWhatsapp, Checklist, CircleCheck} from 'tabler-icons-react'
 import { Button } from '@mantine/core'
 import Buttons from '@/components/buttons'
 import { CircleX } from 'tabler-icons-react';
+import {motion} from "framer-motion"
 
 export default function profil() {
     return (
@@ -37,17 +38,17 @@ export default function profil() {
                                 <div>
                                     <TextInput 
                                         icon={<UserCircle size={20}/>} 
-                                        id="nama" 
+                                        id="{nama}" 
                                         withAsterisk={true}
                                         label="Nama Lengkap"
                                         variant='filled'
-                                        placeholder="Moestafa" 
+                                        placeholder="moestop"
                                     />
                                 </div>
                                 <div>
                                     <TextInput
                                         icon={<School size={20}/>}
-                                        label="Asal Sekolah"
+                                        label="Asal Instansi"
                                         id='asal_sekolah'
                                         required
                                         withAsterisk={true}
@@ -79,22 +80,15 @@ export default function profil() {
                                     <Button type="submit" className="w-full bg-dcf-dark-brown hover:bg-dcf-dark-brown/90">Konfirmasi Data</Button>    
                                 </div>
                             </div>
-                            <div className='flex flex-col row-span-3 p-5 bg-white rounded-md'>
-                                <p className="text-[12px] text-black/60">Bukti Pembayaran</p>
+                            <div className='flex flex-col row-span-4 p-5 bg-white rounded-md'>
+                                <p className="text-[12px] text-black/60">Abstrak LKTI</p>
                                 <div className='flex items-center self-center w-4/5 p-10 my-2 border-2 border-dashed rounded-md cursor-pointer border-dcf-dark-brown h-3/5'>
                                         <FileUpload size={50} strokeWidth={1} color={'#967E76'}/>
-                                        <p className='mx-2 text-sm text-dcf-dark-brown'>Klik di sini untuk mengunggah bukti pembayaran</p>
+                                        <p className='mx-2 text-sm text-dcf-dark-brown'>Klik di sini untuk mengunggah Abstrak LKTI</p>
                                 </div>
-                                <Button type="submit" className="self-center w-fit bg-dcf-dark-brown hover:bg-dcf-dark-brown/90">Upload Bukti Pembayaran</Button> 
+                                <Button type="submit" className="self-center w-fit bg-dcf-dark-brown hover:bg-dcf-dark-brown/90">Upload Abstrak LKTI</Button> 
                             </div>
-                            <div className='flex flex-col row-span-3 p-5 bg-white rounded-md'>
-                                <p className="text-[12px] text-black/60">Status Pembayaran</p>
-                                <div className='flex items-center mb-2'>
-                                    <CircleX 
-                                        size={18}
-                                        color='red'/> 
-                                    <p className="mx-2 text-sm">Belum Terverifikasi</p>  
-                                </div>
+                            <div className='flex flex-col row-span-2 p-5 bg-white rounded-md'>
                                 <p className="text-[12px] text-black/60">Status Registrasi</p>
                                 <div className='flex items-center mb-2'>
                                     <CircleX 
@@ -114,23 +108,23 @@ export default function profil() {
                     </div> */}
                     {/* pelaksanaan */}
                     <div className="w-full pb-16 p-7">
-                        <h2 className="my-2 text-xl font-bold">LKTI</h2>
-                        <div className='flex flex-col justify-around h-full'>
-                            <div className="flex flex-col h-full p-5 my-4 overflow-hidden bg-white bg-center bg-cover rounded-md lg:my-2">
+                        <h2 className="my-2 text-xl font-bold">Lomba Karya Tulis Ilmiah</h2>
+                        <div className='flex flex-col justify-around order-1 grid-cols-2 grid-rows-6 gap-4 h-fit lg:h-full lg:grid'>
+                            <div className="flex flex-col h-full row-span-3 row-start-1 p-5 my-4 overflow-hidden bg-white bg-center bg-cover rounded-md shadow-lg lg:my-2 shadow-dcf-dark-brown/30">
                                 <div className='flex justify-between'>
-                                    <p className="text-[12px] text-black/60">Abstrak LKTI</p>
+                                    <p className="text-[12px] text-black/60">Bukti Pembayaran</p>
                                     <p className="self-end text-[12px] text-black/60">Due in 8 days</p>    
                                 </div>
                                 <div className='flex items-center self-center justify-center w-4/5 p-10 my-2 border-2 border-dashed rounded-md cursor-pointer border-dcf-dark-brown h-3/5'>
                                         <FileUpload size={50} strokeWidth={1} color={'#967E76'}/>
-                                        <p className='mx-2 text-sm text-dcf-dark-brown'>Klik di sini untuk mengunggah Abstrak LKTI</p>
+                                        <p className='mx-2 text-sm text-dcf-dark-brown'>Klik di sini untuk mengunggah Bukti Pembayaran</p>
                                 </div>
-                                <Button type="submit" className="self-center w-fit bg-dcf-dark-brown hover:bg-dcf-dark-brown/90">Upload Abstrak LKTI</Button>
+                                <Button type="submit" className="self-center w-fit bg-dcf-dark-brown hover:bg-dcf-dark-brown/90">Upload Bukti Pembayaran</Button>
                             </div>    
-                            <div className="flex flex-col h-full p-5 my-4 overflow-hidden bg-white bg-center bg-cover rounded-md lg:my-2">
+                            <div className="flex flex-col h-full row-span-3 row-start-4 p-5 my-4 overflow-hidden bg-white bg-center bg-cover rounded-md shadow-lg lg:my-2 shadow-dcf-dark-brown/30">
                                 <div className='flex justify-between'>
                                     <p className="text-[12px] text-black/60">Fullpaper LKTI</p>
-                                    <p className="self-end text-[12px] text-black/60">Not started yet</p>    
+                                    <p className="self-end text-[12px] text-black/60">Due in 8 days</p>    
                                 </div>
                                 <div className='flex items-center self-center justify-center w-4/5 p-10 my-2 border-2 border-dashed rounded-md cursor-pointer border-dcf-dark-brown h-3/5'>
                                         <FileUpload size={50} strokeWidth={1} color={'#967E76'}/>
@@ -138,6 +132,43 @@ export default function profil() {
                                 </div>
                                 <Button type="submit" className="self-center w-fit bg-dcf-brown hover:bg-dcf-dark-brown/90">Upload FullPaper LKTI</Button>
                             </div>   
+                            <div className='w-full h-full row-span-4 p-5 bg-white rounded-md shadow-lg shadow-dcf-dark-brown/30'>
+                                <h2 className='w-full p-2 font-bold text-center'>Syarat dan Ketentuan</h2>
+                                <ul>
+                                    <li className='flex text-sm'><CircleCheck className="mt-1 mr-2 shrink-0" size={16} color='green'/><p> Siswa SMA/SMK/sederajat yang masih aktif baik kelas X/XI/XII pada tahun ajaran 2023/2024.</p></li>
+                                    <li className='flex text-sm'><CircleCheck className="mt-1 mr-2 shrink-0" size={16} color='green'/><p> Satu tim terdiri dari 2-3 siswa dari sekolah yang sama dan maksimal tergabung ke dalam 2 tim yang berbeda.</p></li>
+                                    <li className='flex text-sm'><CircleCheck className="mt-1 mr-2 shrink-0" size={16} color='green'/><p> Setiap karya tulis wajib dibimbing oleh seorang guru pembimbing.</p></li>
+                                    <li className='flex text-sm'><CircleCheck className="mt-1 mr-2 shrink-0" size={16} color='green'/><p> Peserta DCF 2023 diwajibkan mengunggah twibbon dan mengikuti akun Instagram @dcfundip</p></li>
+                                </ul>
+                                <div className='flex justify-center p-4'>
+                                    <Button type="submit" className="mx-2 w-fit bg-dcf-dark-brown hover:bg-dcf-dark-brown/90">Buku Panduan</Button>    
+                                    <Button type="submit" className="mx-2 w-fit bg-dcf-dark-brown hover:bg-dcf-dark-brown/90">Petunjuk Teknis</Button>    
+                                </div>
+                                
+                            </div>
+                            <div className='w-full h-full row-span-2 p-5 bg-white rounded-md shadow-lg lg:px-5 lg:py-1 shadow-dcf-dark-brown/30 column-start-2'>
+                                <p className="text-[12px] text-black/60">Status Pembayaran</p>
+                                    <div className='flex items-center mb-2'>
+                                        <CircleX 
+                                            size={16}
+                                            color='red'/> 
+                                        <p className="mx-2 text-sm">Belum Terverifikasi</p>  
+                                    </div>
+                                    <p className="text-[12px] text-black/60">Status Registrasi</p>
+                                    <div className='flex items-center mb-2'>
+                                        <CircleX 
+                                            size={16}
+                                            color='red'/> 
+                                        <p className="mx-2 text-sm">Belum Terverifikasi</p>  
+                                    </div>
+                                    <p className="text-[12px] text-black/60">Kontak</p>
+                                    <div className='flex items-center mb-2'>
+                                        <BrandWhatsapp 
+                                            size={16}
+                                            color='green'/> 
+                                        <p className="mx-2 text-sm">No CP</p>  
+                                    </div>
+                            </div>
                         </div>
                     </div>
                 </div>    
@@ -149,7 +180,6 @@ export default function profil() {
                     </div>
                 </div>
             </div>
-            
         </EmptyLayout>
     )
 }
